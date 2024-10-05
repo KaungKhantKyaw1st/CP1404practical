d6500f8c9054196335b1a1582321bc89ebb16af0
KaungKhantKyaw1st <kaungkhant.kyaw@my.jcu.edu.au>
2024-10-05 21:32:03 +0800

    Update exceptions_to_complete.py

--- prac_03/exceptions_to_complete.py
+++ prac_03/exceptions_to_complete.py
@@ -1,1 +1,16 @@
+"""
+CP1404/CP5632 - Practical
+Fill in the TODOs to complete the task
+"""
+
+is_finished = False  # Flag to indicate when to exit the loop
+
+while not is_finished:
+    try:
+        result = int(input("Enter a valid integer: "))  # Get user input and convert to integer
+        is_finished = True  # Set flag to True to exit the loop if input is valid
+    except ValueError:  # Catch ValueError if conversion fails
+        print("Please enter a valid integer.")  # Prompt the user to try again
+
+print(f"Valid result is: {result}")  # Output the valid integer
 
